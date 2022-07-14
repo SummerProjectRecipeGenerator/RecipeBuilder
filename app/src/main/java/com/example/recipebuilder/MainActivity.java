87805fcb79e1abb637efb4f3aa080dbb70d363b8
Devdeltek <39629179+Devdeltek@users.noreply.github.com>
2022-07-14 16:53:47 -0700

    Added comment

--- app/src/main/java/com/example/recipebuilder/MainActivity.java
+++ app/src/main/java/com/example/recipebuilder/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
 
-    //Devin Was here
+    //Devin Was her
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
